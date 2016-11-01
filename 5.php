417114614c4cdd96814b5d5ea74d5fa188924634
PauLo <paul.bodenchuk@gmail.com>
2016-11-01 18:59:44 +0200

    corrections from 01/11/2016

--- 5.php
+++ 5.php
@@ -1,11 +1,12 @@
 <?php
 $arr = array('Коля'=>'200', 'Вася'=>'300','Петя'=>'400');
-/*
+
 while (current($arr))
 {
     echo key($arr).' - зарплата '.$arr[key($arr)].' долларов<br>';
     next($arr);
 }
+/*
 foreach ($arr as $key => $value){
     echo $key.' - зарплата '.$value.' долларов<br>';
 }
--- 6.php
+++ 6.php
@@ -2,16 +2,16 @@
 $arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
 $count = 0;
 $arr_keys = Array();
+
+
+print_r($arr_keys);
+print_r($arr_values);
 $arr_values = Array();
 foreach ($arr as $key => $value){
     $arr_keys[$count] = $key;
     $arr_values[$count] = $value;
     $count++;
 }
-
-print_r($arr_keys);
-print_r($arr_values);
-
 /*
 $arr_keys = array_keys($arr);
 $arr_values = array_values($arr);
